5643311bbf4d8d992a7b07ee3f9955953dbc2605
nicu <ontanicolae15@gmail.com>
2023-04-08 18:55:48 +0300

    some dumb try

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import { getNotes } from './api/getNotes/getNotes';
+// import { getNotes } from './api/getNotes/getNotes';
+import { cache } from 'react';
 import Form from './Form';
 import Note from './components/Note';
+import prisma from '@/prisma/client';
 // import { postNote } from './api/notes/postNote';
 
 export default async function Home() {
-  const data = await getNotes();
+  // const data = await getNotes();
+  const data = await prisma.note.findMany();
+  fetch(data.toString(), { cache: 'no-store' });
+
   // console.log(data);
 
   return (
-    <main className="grid lg:grid-cols-4 place-items-center">
+    <main className="grid place-items-center lg:grid-cols-4">
       {data.map((note) => (
         <Note
           key={note.id}
