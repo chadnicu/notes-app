448d84fe9a61a5e79f7acffb19f35715fcd38d72
nicu420 <ontanicolae15@gmail.com>
2023-05-09 20:28:38 +0300

    still doesnt work. adding it back

--- lib/actions.ts
+++ lib/actions.ts
@@ -24,7 +24,7 @@ export async function addNote(
       userId,
     },
   });
-  revalidatePath("/notes");  // removed optimistic updates 
+  // revalidatePath("/notes");  // added optimistic updates 
 
   // return new NextResponse(JSON.stringify({ title, content }));
 }
