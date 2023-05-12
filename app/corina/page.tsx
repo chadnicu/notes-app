04ba2a487667aa4fea76c470c1a5c3eb68b713e3
nicu420 <ontanicolae15@gmail.com>
2023-05-12 18:28:00 +0300

    hidden page

---
+++ app/corina/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div className="grid justify-center">
+      {Array(50000)
+        .fill(<p className="text-lg font-bold">O iubesk pe Corina</p>)
+        .map((element, index) => (
+          <div key={index}>{element}</div>
+        ))}
+    </div>
+  );
+}
